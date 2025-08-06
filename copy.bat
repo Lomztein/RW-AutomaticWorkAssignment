9d7d473aef8858fcb7503f63ed0ddc4024eb9b8a
Alexandre GERMAIN-RAFFIER <alexandre.j.p.germain@gmail.com>
2025-08-07 01:34:08 +0200

    build: allow user-configuration of `steamapps` path

--- copy.bat
+++ copy.bat
@@ -1,15 +1,16 @@
-@cd "C:\Users\lomzt\source\repos\RW-AutomaticWorkAssignment"
-@set name=%1
+@set slnDir=%1
+@cd "%slnDir%"
+@set name=%2
 
 @echo Copying mod %name% to RimWorld mods
 
-@set dest=%2%
+@set dest=%3%
 @set dest=%dest:"=%
 
 @echo %name%
 @echo %dest%
 
 @del "%dest%/%name%" /q
-@robocopy "./%1" "%dest%/%name%" /xd "obj" "bin" /xf "*.csproj" /mir
+@robocopy "./%2" "%dest%/%name%" /xd "obj" "bin" /xf "*.csproj" /mir
 
 @exit /B
