9aa3072a54ee24a6887fc96b03c522abe6c146e6
Alexandre GERMAIN-RAFFIER <alexandre.j.p.germain@gmail.com>
2025-08-10 02:42:34 +0200

    feat: basic operator without priority

--- AutomaticWorkAssignment/Source/PawnFitness/FormulaPawnFitness.Parser.cs
+++ AutomaticWorkAssignment/Source/PawnFitness/FormulaPawnFitness.Parser.cs
@@ -1,6 +1,10 @@
+﻿using RimWorld;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq.Expressions;
+using Verse;
+using static Lomzie.AutomaticWorkAssignment.PawnFitness.FormulaPawnFitness.Parser;
 
 namespace Lomzie.AutomaticWorkAssignment.PawnFitness
 {
@@ -140,6 +144,117 @@ namespace Lomzie.AutomaticWorkAssignment.PawnFitness
                     {
                         throw new ArgumentException($"Invalid character: '{formula[i]}' (formula is \"{formula}\", invalid at index {i})", nameof(formula));
                     }
+                }
+            }
+            #endregion
+
+            #region Parse
+            interface IExpression { }
+            readonly struct LiteralExpression : IExpression
+            {
+                public readonly double Value;
+            }
+            readonly struct FunctionCallExpression : IExpression
+            {
+                public readonly string Name;
+                public readonly IExpression[] Arguments;
+            }
+            readonly struct ArgumentExpression : IExpression
+            {
+                public readonly string Name;
+            }
+            readonly struct BinaryOperationExpression: IExpression
+            {
+                public readonly IExpression Left;
+                public readonly IExpression Right;
+            }
+            public delegate double CalcFitness(Pawn pawn, WorkSpecification specification, ResolveWorkRequest request);
+            public class Formula {
+                public Formula(Expression<CalcFitness> expression)
+                {
+                    Expression = expression;
+                }
+
+                public Expression<CalcFitness> Expression { get; }
+                public float Calc(Pawn pawn, WorkSpecification specification, ResolveWorkRequest request)
+                {
+                    var fn = Expression.Compile();
+                    return (float)fn(pawn, specification, request);
+                }
+            }
+            internal class Context
+            {
+                private static readonly ParameterExpression pawnParameter = Expression.Parameter(typeof(Pawn), "pawn");
+                private static readonly ParameterExpression specificationParameter = Expression.Parameter(typeof(WorkSpecification), "specification");
+                private static readonly ParameterExpression requestParameter = Expression.Parameter(typeof(ResolveWorkRequest), "request");
+                private Expression? root;
+
+                internal Context ParseTokens(IEnumerable<IToken> tokens)
+                {
+                    Expression parent = null;
+                    Action<Expression> yield = prev =>
+                    {
+                        parent = prev;
+                    };
+
+                    foreach (var current in tokens)
+                    {
+                        switch (current)
+                        {
+                            case NumberToken numberToken:
+                                var expr = Expression.Constant(numberToken.Value, typeof(double));
+                                yield(expr);
+                                yield = null;
+                                break;
+
+                            case OperatorToken operatorToken:
+                                Expression currentPrev = parent;
+                                switch (operatorToken.Value)
+                                {
+                                    case Operator.Factor:
+                                        yield = (next) =>
+                                        {
+                                            if (next == null)
+                                            {
+                                                throw new InvalidOperationException("prev");
+                                            }
+                                            parent = Expression.Multiply(currentPrev, next);
+                                        };
+                                        break;
+                                    case Operator.Sum:
+                                        yield = (next) =>
+                                        {
+                                            if (next == null)
+                                            {
+                                                throw new InvalidOperationException("prev");
+                                            }
+                                            Expression.Add(currentPrev, next);
+                                            parent = Expression.Add(currentPrev, next);
+                                        };
+                                        break;
+                                    default:
+                                        throw new NotImplementedException($"Operator {Enum.GetName(typeof(Operator), operatorToken.Value)} not supported");
+                                }
+                                break;
+
+                            default:
+                                throw new ArgumentException("Invalid token type", nameof(current));
+
+                        }
+                    }
+                    root = parent;
+                    return this;
+                }
+
+                internal Formula ToFormula()
+                {
+                    if (root == null)
+                    {
+                        throw new InvalidOperationException("No tokens");
+                    }
+                    var body = root;
+                    var callExpr = Expression.Lambda<CalcFitness>(body, pawnParameter, specificationParameter, requestParameter);
+                    return new Formula(callExpr);
                 }
             }
             #endregion
