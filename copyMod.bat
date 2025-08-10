d3ab79767d1c511ab6daaab25f2f772002024584
Alexandre GERMAIN-RAFFIER <alexandre.j.p.germain@gmail.com>
2025-08-10 18:59:42 +0200

    build: do not hang build on failed copy, low warning instead

--- copyMod.bat
+++ copyMod.bat
@@ -4,9 +4,13 @@
 
 @echo Copying mod %name% to RimWorld mods (%dest%)
 
-@robocopy "./%name%" "%dest%" /xd "obj" "bin" /xf "*.csproj" "*.cs" /s
+@(robocopy "./%name%" "%dest%" /xd "obj" "bin" /xf "*.csproj" "*.cs" /s /r:0 /w:1)
+@if %ERRORLEVEL% GEQ 8 (
+    @echo Copy failed 1>&2
+    @exit 1
+)
 :: Prune dirs
-cd "%dest%"
-for /f "delims=" %%d in ('dir /s /b /ad ^| sort /r') do @rd "%%d" 2>NUL
+@cd "%dest%"
+@for /f "delims=" %%d in ('dir /s /b /ad ^| sort /r') do @rd "%%d" 2>NUL
 
 @exit /B
--- copyModPatch.bat
+++ copyModPatch.bat
@@ -6,9 +6,15 @@
 @echo Copying patch %name% for %basemod% to RimWorld mods (%dest%)
 
 @cd "./%name%"
-for /f %%d in ('dir /b /ad ^| sort /r ^|  findstr /E /R "[0-9]\.[0-9]"') do @robocopy ".\%%d" "%dest:"=%\%%d\Mods\%name:"=%" /s
+@for /f %%d in ('dir /b /ad ^| sort /r ^|  findstr /E /R "[0-9]\.[0-9]"') do (
+    @(robocopy ".\%%d" "%dest:"=%\%%d\Mods\%name:"=%" /s /r:0 /w:1)
+    @if %ERRORLEVEL% GEQ 8 (
+        @echo Copy failed 1>&2
+        @exit 1
+    )
+)
 :: Prune dirs
-cd "%dest%"
-for /f "delims=" %%d in ('dir /s /b /ad ^| sort /r') do @rd "%%d" 2>NUL
+@cd "%dest%"
+@for /f "delims=" %%d in ('dir /s /b /ad ^| sort /r') do @rd "%%d" 2>NUL
 
 @exit /B
