4af5c08a658aab0d562f8fccc111f87a1f849f73
Alexandre GERMAIN-RAFFIER <alexandre.j.p.germain@gmail.com>
2025-08-10 04:47:24 +0200

    feat: implement ugly ui handler

---
+++ AutomaticWorkAssignment/Source/PawnFitness/FormulaPawnFitness.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace Lomzie.AutomaticWorkAssignment.PawnFitness
+{
+    public partial class FormulaPawnFitness : PawnSetting, IPawnFitness
+    {
+        public string sourceString;
+        private string _commitedString;
+        private Parser.Formula? _formula;
+        internal Parser.Formula? Formula => _formula;
+        private string _CommitedFormula { get => _commitedString; set
+            {
+                if (value != _commitedString)
+                {
+                    Logger.Message($"[AWA:core:Formula] Loading {value}");
+                    _formula = new Parser().ParseFormula(value);
+                    Logger.Message($"[AWA:core:Formula] Loaded {_formula.Expression} with bindings [{string.Join(", ", _formula.BindingNames)}]");
+                    var obsoleteBindings = bindingSettings.Keys.Except(_formula.BindingNames).ToArray();
+                    Logger.Message($"[AWA:core:Formula] Clearing obsolete bindings [{string.Join(", ", obsoleteBindings)}]");
+                    bindingSettings.RemoveRange(obsoleteBindings);
+                    _commitedString = value;
+                    sourceString = value;
+                } else
+                {
+                    Logger.Message($"[AWA:core:Formula] Skip loading");
+                }
+            }
+        }
+        public Dictionary<string, IPawnFitness> bindingSettings = new();
+
+        internal void Commit()
+        {
+            _CommitedFormula = sourceString;
+        }
+
+        public float CalcFitness(Pawn pawn, WorkSpecification specification, ResolveWorkRequest request)
+        {
+            return 0f;
+        }
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            var commited = _CommitedFormula;
+            Scribe_Values.Look(ref commited, "formula");
+            Scribe_Collections.Look(ref bindingSettings, "bindings");
+            _CommitedFormula = commited;
+        }
+    }
+}
---
+++ AutomaticWorkAssignment/Source/UI/PawnFitness/FormulaPawnFitnessUIHandler.cs
@@ -0,0 +1,79 @@
+﻿using Lomzie.AutomaticWorkAssignment.Defs;
+using Lomzie.AutomaticWorkAssignment.GenericPawnSettings;
+using Lomzie.AutomaticWorkAssignment.PawnFitness;
+using RimWorld;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace Lomzie.AutomaticWorkAssignment.UI.PawnFitness
+{
+    public class FormulaPawnFitnessUIHandler : PawnSettingUIHandler<FormulaPawnFitness>
+    {
+        private static readonly float _inputFieldSize = 32;
+        private static readonly float _buttonSize = 16;
+        private static readonly float _labelSize = 24;
+
+        protected override float Handle(Vector2 position, float width, FormulaPawnFitness pawnSetting)
+        {
+            var localPosition = position;
+            Rect rect = new Rect(localPosition, new Vector2(width, _inputFieldSize));
+            var newFormula = Widgets.TextField(rect, pawnSetting.sourceString);
+            localPosition.y += rect.height;
+            if (newFormula != pawnSetting.sourceString)
+            {
+                Logger.Message($"[AWA:core] New text: {newFormula}");
+                pawnSetting.sourceString = newFormula;
+            }
+            Rect buttonRect = new Rect(localPosition, new Vector2(width, _buttonSize));
+            if (Widgets.ButtonText(buttonRect, "Commit"))
+            {
+                Logger.Message($"[AWA:core] Commit: {pawnSetting.sourceString}");
+                pawnSetting.Commit();
+            }
+            localPosition.y += _buttonSize;
+            if (pawnSetting.Formula != null)
+            {
+                localPosition.x += 4;
+                width -= 4;
+                foreach (var bindingName in pawnSetting.Formula.BindingNames)
+                {
+                    Rect labelRect = new Rect(localPosition, new Vector2(width, _labelSize));
+                    localPosition.y += _labelSize;
+                    Widgets.Label(labelRect, bindingName);
+                    if (pawnSetting.bindingSettings.TryGetValue(bindingName, out var setting))
+                    {
+                        localPosition.y += WorkManagerWindow.DoPawnSetting(
+                            position: localPosition,
+                            width: width,
+                            setting: setting,
+                            onMoveSetting: null,
+                            onDeleteSetting: (x) => Find.Root.StartCoroutine(DelayedRemoveInnerSetting(bindingName, pawnSetting)));
+                    }
+                    else
+                    {
+                        Rect addConditionButtonRect = new Rect(localPosition, new Vector2(width, _inputFieldSize));
+                        localPosition.y += _inputFieldSize;
+                        if (Widgets.ButtonText(addConditionButtonRect, "AWA.NestedSettingSelect".Translate()))
+                        {
+                            FloatMenuUtility.MakeMenu(
+                                objects: GetDefs(),
+                                labelGetter: def => def.LabelCap,
+                                actionGetter: def => () => pawnSetting.bindingSettings.SetOrAdd(bindingName, PawnSetting.CreateFrom<IPawnFitness>(def)));
+                        }
+                    }
+                }
+            }
+            return localPosition.y - position.y;
+        }
+
+        private IEnumerable<PawnFitnessDef> GetDefs()
+            => DefDatabase<PawnFitnessDef>.AllDefs;
+        private IEnumerator DelayedRemoveInnerSetting(string name, FormulaPawnFitness setting)
+        {
+            yield return new WaitForEndOfFrame();
+            setting.bindingSettings.Remove(name);
+        }
+    }
+}
