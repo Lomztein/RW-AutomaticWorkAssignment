9d214f789abb41a5d5278f3f825e6ecf44eaa01f
SeanWoo <kokarevvadim2002@gmail.com>
2026-09-09 15:43:10 +0800

    Keep regression harness local

--- Addon_Events.Tests/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-cd "$(dirname "$0")"
-output=$(mktemp -d)
-trap 'rm -rf "$output"' EXIT
-mcs -langversion:latest -out:"$output/tests.exe" CoroutineRegressionTests.cs ../AutomaticWorkAssignment/Source/Buffer.cs ../Addon_Events/Source/PawnPostProcessors/DoOnConditionChangedPostProcessor.cs ../Addon_Events/Source/PawnPostProcessors/DoRepeatPostProcessor.cs
-mono "$output/tests.exe"
